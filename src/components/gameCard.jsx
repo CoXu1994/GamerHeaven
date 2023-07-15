c2fd8177d0cbd582b3947ac71bae9c2f77b90209
CoXu1994 <mateusz.radomski1994@gmail.com>
2023-07-15 18:46:49 +0200

    Finished styling game details card, for mobile, tablet and desktop version, sass usage

--- src/components/gameCard.jsx
+++ src/components/gameCard.jsx
@@ -3,7 +3,7 @@ import { getGameApi } from "../api/api";
 import { useState, useEffect } from "react";
 import { Button } from "@mui/material";
 import { AddToWishList } from "./wishlistOperations";
-import gameCard.scss;
+import "./gameCard.scss";
 
 function GameCard() {
    const {gameId} = useParams();
@@ -23,119 +23,49 @@ function GameCard() {
    const {id, name, description_raw: description , esrb_rating, metacritic, platforms, background_image : image, background_image_additional : bgc_image} = game;
     return (
 
-        <div className="game_container" key={id} 
-            style={{
-                
-                marginTop: 50,  
-                width: 375, 
-                display: "flex", 
-                flexDirection: "column", 
-                alignItems: "center", 
-                justifyContent: "center",
-                marginInline: "auto",
-                
-            }}>
-            <div>
-                <img 
-                    style={{
-                        marginTop: 20,
-                        width: 360, 
-                        height: 180, 
-                        borderRadius: 5,
-                    }}
-                    src={image} 
-                    alt="image_of_game" 
-                />
-                <div style={{
-                    backgroundColor: "rgba(255,255,255, 0.125)", 
-                    borderRadius: 5, 
-                    width: 360, 
-                    textAlign: "center", 
-                    color: "rgba(255,255,255)", 
-                    padding: "1px 10px", 
-                    boxSizing: "border-box",
-                    marginTop: 15,
-                    }}>
-                    <h2 style= {{fontFamily: `'Tektur', cursive`, fontSize: 22, fontWeight: 600 }}>{name}</h2>
-                </div>
-
-                <div style={{
-                    backgroundColor: "rgba(255,255,255, 0.125)", 
-                    borderRadius: 5, 
-                    width: 360,  
-                    color: "rgba(255,255,255)", 
-                    padding: "6px 10px", 
-                    marginTop: 10
-                }}>
-                <span style={{
-                    display: "block",
-                    borderBottom: "1px solid white",
-                    fontSize: 20,
-                    lineHeight: 1.5,
-                    fontFamily: `'Tektur', cursive`,
-                    fontWeight: 500
-                }}>Description</span>
-                <p style={{
-                    overflow: "auto",
-                    height: 150,
-                    color: "white",
-                    marginTop: 10,
-                    fontFamily: `'Tektur', cursive`,
-                    fontWeight: 400,
-                    fontSize: 14,
-                    paddingBottom: 6
-                    }}>{description}</p>
-                
-                </div>              
+        <div className="gameCard__container--main" key={id} >
+            <img className="gameCard__image" src={image} alt="image_of_game"/>
+            <div className="gameCard__title--game">
+                <h2 className="gameCard__title">{name}</h2>
             </div>
-            <div style={{ 
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                marginInline: "auto",
-                gap: 5,
-                marginTop: 15, 
-                width: 360,
-                padding: 10,
-                backgroundColor: "rgba(255,255,255, 0.125)", 
-                fontFamily: `'Tektur', cursive`}}>
-                <div style={{display: "flex",flexBasis: "40%", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: 3}}>
-                    <span style= {{fontWeight: "bold",fontSize: 20, color: "white", borderBottom: "1px solid white", lineHeight: 2}}>ESRB rating: </span>
-                    <span style={{color: "white", marginTop: 10}}>{esrb_rating.name}</span>
-                    
+
+            <div className="gameCard__container--description">
+                <h3 className="gameCard__description__title">Description</h3>
+                <p className="gameCard__description__txt">{description}</p>           
+            </div>
+
+            <div className="gameCard__container--rates">
+                <div className="gameCard__esrb">
+                    <span className="gameCard__esrb__title">ESRB rating: </span>
+                    <span className="gameCard__esrb__rate">{esrb_rating.name}</span>    
                 </div>
-                <div style={{marginLeft: 20,display: "flex",flexBasis: "50%", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: 3}}>
-                    <span style={{fontWeight: "bold",fontSize: 20, color: "white", borderBottom: "1px solid white", lineHeight: 2}}>Metacritic Score: </span>
-                    <span style = {{fontWeight: 400, color: "white", marginTop: 10}}>{metacritic}</span>
+                <div className="gameCard__metascore">
+                    <span className="gameCard__metascore__title">Metacritic Score: </span>
+                    <span className="gameCard__metascore__score">{metacritic}</span>
                 </div>                   
             </div>
-            <div style={{ 
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                marginInline: "auto",
-                gap: 5,
-                marginTop: 15, 
-                marginBottom: 25,
-                width: 360,
-                padding: 10,
-                backgroundColor: "rgba(255,255,255, 0.125)", 
-                fontFamily: `'Tektur', cursive`}}>
+            <div className="gameCard__platforms">
                 <div>
-                    <h3 style= {{textAlign: "center", fontWeight: "bold",fontSize: 20, color: "white", borderBottom: "1px solid white", lineHeight: 2}}>Platforms: </h3>    
-                    <div style= {{textAlign: "center", color: "white", marginTop: 10}}>
+                    <h3 className="gameCard__platforms__title">Platforms: </h3>    
+                    <div className="gameCard__platforms__txt">
                         - {platforms.map((item) => (
                             <span>{item.platform.name} - </span>
                         ))}
-                    </div>
-                    
-                </div>    
-                
+                    </div>   
+                </div>      
             </div>
-            <Button onClick={() => AddToWishList(game)} sx={{color: "white", borderColor: "white", marginBottom: 5}} variant="outlined">
-               Add to Wishlist</Button>
-        </div>
-        
+            <Button className="button"
+                onClick={() => AddToWishList(game)} 
+                sx={{
+                    color: "white", 
+                    borderColor: "white",
+                    marginTop: 5, 
+                    marginBottom: 5,
+                }} 
+                variant="outlined">
+               Add to Wishlist
+            </Button>
+        </div>  
     )
 }
 
